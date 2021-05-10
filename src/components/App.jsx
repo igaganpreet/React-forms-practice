4bdde1d3073a6e7546c128dfddc58926e8f80295
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-10 23:59:01 +0530

    display input in heading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("hello");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    let nw = event.target.value;
+    setHeading("hello " + nw);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>{heading}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
