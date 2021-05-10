074f02c893f8070577a018afd73d415a3326ca39
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-11 01:16:03 +0530

    prevent default in form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,29 @@ function App() {
   const [name, setName] = useState("");
   const [headingText, setHeading] = useState("");
 
-  let newName;
   function handleChange(event) {
     console.log(event.target.value);
-    newName = event.target.value;
+    let newName = event.target.value;
     setName(newName);
   }
+
   function handleClick(event) {
-    console.log("clicked");
     setHeading(name);
+    event.preventDefault();
   }
+
   return (
     <div className="container">
       <h1>Hello {headingText}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-        value={name}
-      />
-      <button onClick={handleClick}>Submit</button>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
